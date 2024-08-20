a73fe62b002a0fc83b68169977d0a0677eb4a037
bnwDunk <teerapong002588@gmail.com>
2024-08-21 00:48:58 +0700

    version1.0

---
+++ db/db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE db-dev;
+USE db-dev;
+
+CREATE TABLE users (
+    uid int not null AUTO_INCREMENT,
+    name varchar(100) not null,
+    age int (100) not null,
+    PRIMARY KEY (uid)
+);
+
+INSERT INTO users(name, age)
+VALUES("Dunk", "22"), ("Just_test","18");
